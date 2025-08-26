7459492a3d36f24b83c60bff56b0869a164aba72
Jade Cestari <Jade_cestari@hotmail.com>
2025-08-26 09:40:37 -0300

    Update main.c

--- ex-1.3.1/main.c
+++ ex-1.3.1/main.c
@@ -5,27 +5,37 @@ int main(int argc, char* args[]) {
     SDL_Window* win = SDL_CreateWindow("ex-1.3.1",
                          SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED,
-                         200, 200, SDL_WINDOW_SHOWN);
+                         400, 400, SDL_WINDOW_SHOWN);
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
-    SDL_Rect r = {90, 90, 20, 20};
-    int step = 10;
-    int dir = 0;  
+    SDL_Rect r = {190, 100, 20, 20}; 
+
+    int step = 1;       
+    int dir = 0;       
+    int count = 0;      
+    int side = 90;      
 
     while (1) {
+       
         SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
         SDL_RenderClear(ren);
 
-        SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+        SDL_SetRenderDrawColor(ren, 0x00,0x00,0x00,0x00);
         SDL_RenderFillRect(ren, &r);
         SDL_RenderPresent(ren);
 
-        SDL_Delay(100);
+        SDL_Delay(10);
 
-        if (dir == 0) { r.x += step; r.y += step; if (r.x >= 110) dir = 1; }
-        else if (dir == 1) { r.x -= step; r.y += step; if (r.y >= 110) dir = 2; }
-        else if (dir == 2) { r.x -= step; r.y -= step; if (r.x <= 90) dir = 3; }
-        else if (dir == 3) { r.x += step; r.y -= step; if (r.y <= 90) dir = 0; }
+        if (dir == 0) r.y += step;      
+        else if (dir == 1) r.x += step;  
+        else if (dir == 2) r.y -= step;  
+        else if (dir == 3) r.x -= step;  
+
+        count += step;
+        if (count >= side) {
+            dir = (dir + 1) % 4;
+            count = 0;
+        }
     }
 
     SDL_DestroyRenderer(ren);
@@ -33,4 +43,3 @@ int main(int argc, char* args[]) {
     SDL_Quit();
     return 0;
 }
-
