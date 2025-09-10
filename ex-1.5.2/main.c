6ce5a0e017130084bba1ca67bed017b754a4ef05
Jade Cestari <Jade_cestari@hotmail.com>
2025-09-10 18:35:29 -0300

    Update main.c

--- ex-1.5.2/main.c
+++ ex-1.5.2/main.c
@@ -1,30 +1,32 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
-int AUX_WaitEventTimeoutCount(SDL_Event* evt, Uint32* ms) {
+int AUX_WaitEventTimeout(SDL_Event* evt, Uint32* ms) {
     Uint32 inicio = SDL_GetTicks();
-    int resultado = SDL_WaitEventTimeout(evt, *ms);
+    int ret = SDL_WaitEventTimeout(evt, *ms);
     Uint32 fim = SDL_GetTicks();
     Uint32 decorrido = fim - inicio;
+
     if (decorrido >= *ms) {
         *ms = 0;
     } else {
         *ms -= decorrido;
     }
-    return resultado;
+    return ret;
 }
 
 int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+
     SDL_Window* window = SDL_CreateWindow("ex-1.5.2",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
                                           200, 300, 0);
- 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-    SDL_Rect rect1 = {20, 50, 40, 40};  
-    SDL_Rect rect2 = {80, 50, 40, 40};  
-    SDL_Rect rect3 = {140, 50, 40, 40};  
+    SDL_Rect rect1 = {20, 50, 40, 40};
+    SDL_Rect rect2 = {80, 50, 40, 40};
+    SDL_Rect rect3 = {140, 50, 40, 40};
 
     int velocidade = 3;
     bool rodando = true;
@@ -33,10 +35,9 @@ int main(int argc, char* argv[]) {
     Uint32 tempoRestante = 50;
 
     while (rodando) {
-        if (AUX_WaitEventTimeoutCount(&event, &tempoRestante)) {
-            if (event.type == SDL_QUIT) {
+        if (AUX_WaitEventTimeout(&event, &tempoRestante)) {
+            if (event.type == SDL_QUIT)
                 rodando = false;
-            }
             if (event.type == SDL_KEYDOWN) {
                 if (event.key.keysym.sym == SDLK_ESCAPE)
                     rodando = false;
@@ -72,8 +73,6 @@ int main(int argc, char* argv[]) {
         SDL_RenderFillRect(renderer, &rect3);
 
         SDL_RenderPresent(renderer);
-
-        SDL_Delay(10);
     }
 
     SDL_DestroyRenderer(renderer);
