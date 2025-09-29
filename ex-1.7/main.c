0f5c289e2e7bd27c84ca9bd4013b01ea48bd7f59
Jade Cestari <Jade_cestari@hotmail.com>
2025-09-29 18:40:11 -0300

    Update main.c

--- ex-1.7/main.c
+++ ex-1.7/main.c
@@ -1,10 +1,20 @@
 #include <assert.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <stdbool.h>
+
+bool AUX_WaitEventTimeout(SDL_Event* e, int timeout_ms) {
+    Uint32 start = SDL_GetTicks();
+    while ((SDL_GetTicks() - start) < (Uint32)timeout_ms) {
+        if (SDL_WaitEventTimeout(e, timeout_ms - (SDL_GetTicks() - start))) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main (int argc, char* args[])
 {
-    /* INICIALIZACAO */
     SDL_Init(SDL_INIT_EVERYTHING);
     IMG_Init(0);
     SDL_Window* win = SDL_CreateWindow("ex-1.7",
@@ -16,27 +26,37 @@ int main (int argc, char* args[])
     SDL_Texture* img = IMG_LoadTexture(ren, "pikachu.png");
     assert(img != NULL);
 
-    /* EXECUÇÃO */
     int isup = 1;
     int x = -100;
-    while (1) {
+    Uint32 lastTime = SDL_GetTicks();
+    SDL_Event event;
+    bool rodando = true;
+
+    while (rodando) {
+        AUX_WaitEventTimeout(&event, 10);
+        if (event.type == SDL_QUIT) break;
+
+        Uint32 now = SDL_GetTicks();
+        if (now - lastTime >= 250) {
+            x = (x + 10) % 400;
+            isup = !isup;
+            lastTime = now;
+        }
+
         SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
         SDL_RenderClear(ren);
+
         SDL_Rect r = { x,50, 100,80 };
         SDL_Rect c;
         if (isup) {
-            c = (SDL_Rect) {   0,0, 100,80 };
+            c = (SDL_Rect) { 0,0, 100,80 };
         } else {
             c = (SDL_Rect) { 100,0, 100,80 };
         }
         SDL_RenderCopy(ren, img, &c, &r);
         SDL_RenderPresent(ren);
-        SDL_Delay(250);
-        x = (x + 10) % 400;
-        isup = !isup;
     }
 
-    /* FINALIZACAO */
     SDL_DestroyTexture(img);
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
