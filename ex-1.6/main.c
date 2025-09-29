cbf87d1c55a533677e7ae23fa5c0e5b9408102a8
Jade Cestari <Jade_cestari@hotmail.com>
2025-09-29 18:32:58 -0300

    Update main.c

--- ex-1.6/main.c
+++ ex-1.6/main.c
@@ -5,50 +5,48 @@
 #include <stdlib.h>
 #include <time.h>
 
+bool AUX_WaitEventTimeout(SDL_Event* e, int timeout_ms) {
+    Uint32 start = SDL_GetTicks();
+    while ((SDL_GetTicks() - start) < (Uint32)timeout_ms) {
+        if (SDL_WaitEventTimeout(e, timeout_ms - (SDL_GetTicks() - start))) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
     srand(time(NULL));
 
-    SDL_Window* window = SDL_CreateWindow("ex-1.6",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          640, 200, 0);
+    SDL_Window* window = SDL_CreateWindow("ex-1.6", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 200, 0);
     SDL_Renderer* ren = SDL_CreateRenderer(window, -1, 0);
 
-    SDL_SetRenderDrawBlendMode(ren, SDL_BLENDMODE_BLEND);
-
     TTF_Font* fonte = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24);
-  
 
     int linhaChegadaX = 500;
-    SDL_Rect rect1, rect2, rect3;
-    int velocidade1, velocidade2, velocidade3;
-    bool chegou1, chegou2, chegou3;
-    int vencedor;
-
-    rect1 = (SDL_Rect){20, 30, 40, 40};
-    rect2 = (SDL_Rect){20, 80, 40, 40};
-    rect3 = (SDL_Rect){20, 130, 40, 40};
-    velocidade1 = rand()%4 + 2;
-    velocidade2 = rand()%4 + 2;
-    velocidade3 = rand()%4 + 2;
-    chegou1 = chegou2 = chegou3 = false;
-    vencedor = 0;
-
+    SDL_Rect rect1 = {20, 30, 40, 40};
+    SDL_Rect rect2 = {20, 80, 40, 40};
+    SDL_Rect rect3 = {20, 130, 40, 40};
+    int velocidade1 = rand()%4 + 2;
+    int velocidade2 = rand()%4 + 2;
+    int velocidade3 = rand()%4 + 2;
+    bool chegou1 = false, chegou2 = false, chegou3 = false;
+    int vencedor = 0;
     bool rodando = true;
     SDL_Event event;
 
     while (rodando) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) rodando = false;
-            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
-                rodando = false;
-        }
+        AUX_WaitEventTimeout(&event, 10);
+        if (event.type == SDL_QUIT) rodando = false;
+        if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
+            rodando = false;
 
         if (!chegou1) {
             rect1.x += velocidade1;
             if (rect1.x + rect1.w >= linhaChegadaX) {
+                rect1.x = linhaChegadaX - rect1.w;
                 chegou1 = true;
                 if (vencedor == 0) vencedor = 1;
             }
@@ -56,6 +54,7 @@ int main(int argc, char* argv[]) {
         if (!chegou2) {
             rect2.x += velocidade2;
             if (rect2.x + rect2.w >= linhaChegadaX) {
+                rect2.x = linhaChegadaX - rect2.w;
                 chegou2 = true;
                 if (vencedor == 0) vencedor = 2;
             }
@@ -63,6 +62,7 @@ int main(int argc, char* argv[]) {
         if (!chegou3) {
             rect3.x += velocidade3;
             if (rect3.x + rect3.w >= linhaChegadaX) {
+                rect3.x = linhaChegadaX - rect3.w;
                 chegou3 = true;
                 if (vencedor == 0) vencedor = 3;
             }
@@ -70,10 +70,8 @@ int main(int argc, char* argv[]) {
 
         SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(ren);
-
         SDL_SetRenderDrawColor(ren, 0x00, 0x00, 0x00, 0xFF);
         SDL_RenderDrawLine(ren, linhaChegadaX, 0, linhaChegadaX, 200);
-
         SDL_SetRenderDrawColor(ren, 0xFF, 0x00, 0x00, 0xFF);
         SDL_RenderFillRect(ren, &rect1);
         SDL_SetRenderDrawColor(ren, 0x00, 0xFF, 0x00, 0xFF);
@@ -91,7 +89,16 @@ int main(int argc, char* argv[]) {
             SDL_FreeSurface(surf);
             SDL_RenderCopy(ren, tex, NULL, &dst);
             SDL_RenderPresent(ren);
-            SDL_Delay(2000);
+
+            Uint32 start = SDL_GetTicks();
+            while (SDL_GetTicks() - start < 2000) {
+                AUX_WaitEventTimeout(&event, 10);
+                if (event.type == SDL_QUIT) {
+                    rodando = false;
+                    break;
+                }
+            }
+
             SDL_DestroyTexture(tex);
             rect1.x = rect2.x = rect3.x = 20;
             chegou1 = chegou2 = chegou3 = false;
@@ -102,8 +109,6 @@ int main(int argc, char* argv[]) {
         } else {
             SDL_RenderPresent(ren);
         }
-
-        SDL_Delay(10);
     }
 
     TTF_CloseFont(fonte);
